555f5bc16b342cf2bde9907a5bf209323ee040fc
wangwang624 <3448337653@qq.com>
2025-05-13 09:03:22 +0800

    Update express.js
    更新服务的监听端口。由 3000 更改为 8080 端口
    并额外添加了注释说明

--- express.js
+++ express.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+
+// 编写不同请求的请求路径
 app.get('/',(req,res)=>{
 	res.send('<h1>你好，Hello World！</h1>');
 });
@@ -10,6 +12,7 @@ app.post('/',(req,res)=>{
 	res.send('<h1>你好，Hello World！</h1>');
 });
 
-app.listen(3000,()=>{
+// 服务的监听端口
+app.listen(8080,()=>{
 	console.log('服务器已启动，监听端口为：3000');
 });
